38ac7e929908b8c34a7b172de09b256e45c4314c
Sarah Pimentel <pimentel.sarah@escola.pr.gov.br>
2025-06-13 19:53:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'Biologia',
+    'O que a biologia estuda?',
+    'A biologia é a ciência que estuda a vida, abrangendo desde os processos biológicos mais básicos em nível molecular até a interação de organismos complexos com o meio ambiente.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são as 5 áreas da biologia?',
+    'Anatomia, Biologia celular, Bioquímica e Botânica e Zoologia'
+)
+criaCartao(
+    'Biologia Celular',
+    'As células eucariontes podem ser classificadas em dois grupos principais, quais são eles?',
+    'Células animais e células vegetais'
+)
+criaCartao(
+    'Biologia Celular',
+    'De acordo com essa teoria celular, que organismo não pode ser considerado um ser vivo?',
+    'De acordo com a teoria celular, os vírus não são seres vivos por não apresentarem células'
+)
+criaCartao(
+    'Biologia',
+    'A primeira lei de Mendel, conhecida também como princípio da segregação dos caracteres ou lei da segregação, afirma que?',
+    'A primeira lei de Mendel afirma que cada característica é condicionada por um par de fatores que se segregam na formação dos gametas, nos quais ocorrem em dose simples'
+)
+criaCartao(
+    'Biologia',
+    'Em seus experimentos, Mendel usou um organismo que apresenta curto tempo de geração, cultivo fácil e que gera grande número de descendentes. Que organismo foi esse?',
+    'A ervilha foi o objeto de estudo de Mendel'
+)
